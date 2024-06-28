3e5c86be18fb97dae0f121511151358c2945e18a
Aryan Dhull <aryandhull879@gmail.com>
2024-06-28 17:56:48 +0530

    backend deployed

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'https://live-polls-system.onrender.com',
+    origin: 'http://localhost:3000',
   },
 });
 
@@ -35,7 +35,7 @@ let studentAnswers = {};
 let connectedUsers = {};
 
 app.use(cors({
-  origin: 'https://live-polls-system.onrender.com'
+  origin: 'http://localhost:3000'
 }));
 
 app.use('/socket.io', (req, res) => {
